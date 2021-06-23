84a646ef313d0fa3fc3bd17f702f74c303fd63a8
aniruddha-garje <garje.avd@gmail.com>
2021-06-23 15:35:52 +0530

    fixed untracked files

--- config/keys-prod.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	MongoURI: process.env.MONGO_URI,
-	GoogleClientID : process.env.GOOGLE_CLIEN_ID,
-	GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-	FacebookClientID : process.env.FACEBOOK_CLIENT_ID,
-	FacebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET
-}
--- public/js/scripts.js
+++
@@ -1,3 +0,0 @@
-window.onload = function(){
-	document.getElementById('header').style.color = 'red';
-}
