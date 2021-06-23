0a7947766da8a3640ce8645a8f0ad9784378d1b2
aniruddha-garje <garje.avd@gmail.com>
2021-06-23 07:31:26 +0530

    deployment

---
+++ config/keys-prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+	MongoURI: 'mongodb+srv://garje:socialmedia@datingapp.4unmy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+	GoogleClientID : '1055245779692-g5dbrd3tkb3a4u0a9rijess2katta66p.apps.googleusercontent.com',
+	GoogleClientSecret: 'SFZv13lWj69su78JKs2UWDpa'
+}
--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-	MongoURI: 'mongodb+srv://garje:socialmedia@datingapp.4unmy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-	GoogleClientID : '708726460747-eff28mah7pac0suo5g7hrgah289872ip.apps.googleusercontent.com',
-	GoogleClientSecret: 'U1kOT-PcKRIUc6CHQG2txX6b'
+if(process.env.NODE_ENV == 'production'){
+	module.exports = require('./keys-prod');
+}else{
+	module.exports = require('./keys-dev');
 }
