951966b9cf39203e4df09f8524031630092f56ea
aniruddha-garje <garje.avd@gmail.com>
2021-06-23 15:34:51 +0530

    fixed untracked files

--- config/keys-dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	MongoURI: 'mongodb+srv://garje:socialmedia@datingapp.4unmy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-	GoogleClientID : '708726460747-eff28mah7pac0suo5g7hrgah289872ip.apps.googleusercontent.com',
-	GoogleClientSecret: 'U1kOT-PcKRIUc6CHQG2txX6b',
-	FacebookClientID: '519634732522486',
-	FacebookClientSecret: 'd555b09101d5457530b46887d692ab9d'
-}
